aed9679c11137644135007a0b9da29747e0d5176
peacepilou <p-l.b@hotmail.com>
2020-08-03 14:04:43 +0200

    Json in progress. PJ are done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PjComponent } from './pages/pj/pj.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PjComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pj/pj.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pj',
+  templateUrl: './pj.component.html',
+  styleUrls: ['./pj.component.scss']
+})
+export class PjComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
