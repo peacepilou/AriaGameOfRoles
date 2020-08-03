e3ab7b9c3d56608f6ee85861d2c7ba1dffc05fb8
lePitch <pierre.henri.jautard@gmail.com>
2020-08-03 16:35:42 +0200

    feat: sevices return all the player array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PjComponent } from './pages/pj/pj.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PjComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
