f0c90e396aa620dceaae28764416a2c7174c1b1f
lePitch <pierre.henri.jautard@gmail.com>
2020-08-05 10:34:26 +0200

    feat: pnj page

---
+++ src/app/pages/pnj/pnj.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pnj',
+  templateUrl: './pnj.component.html',
+  styleUrls: ['./pnj.component.scss']
+})
+export class PnjComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
