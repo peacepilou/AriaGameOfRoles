1069ce74e42aa1c282bc374ba7723ec51e07335a
lePitch <pierre.henri.jautard@gmail.com>
2020-08-06 11:18:57 +0200

    global

--- src/app/commonComponents/global/global.component.ts
+++ src/app/commonComponents/global/global.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
-import { Pj1, Pj, PJ, Global } from 'src/app/models/PJ1';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Global, Caracteristiques, Surnoms } from 'src/app/models/PJ1';
 
 @Component({
   selector: 'app-global',
@@ -8,10 +8,19 @@ import { Pj1, Pj, PJ, Global } from 'src/app/models/PJ1';
 })
 export class GlobalComponent implements OnChanges {
   @Input() global: Global;
-
+  // g: string[] = [];
+  caracteristiques: Caracteristiques;
+  surnoms: Surnoms;
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
+    this.caracteristiques = this.global.caracteristiques;
+    this.surnoms = this.global.surnoms;
+    // for (const key in this.global) {
+      // if (Object.prototype.hasOwnProperty.call(this.global, key)) {
+        // const element = this.global[key];
+        // this.g.push(key + ' : ' + element)
+      // }
+    // }
   }
-
 }
