9096c65f259000e52e7151db3bbf206ab6e0a3e1
lePitch <pierre.henri.jautard@gmail.com>
2020-08-06 14:56:51 +0200

    refactor pj components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PjComponent } from './pages/personnages/pj/pj.component';
-import { GlobalComponent } from './commonComponents/global/global.component';
-import { BiographieComponent } from './commonComponents/biographie/biographie.component';
-import { TitresComponent } from './commonComponents/titres/titres.component';
-import { ModificateursComponent } from './commonComponents/modificateurs/modificateurs.component';
-import { CompetencesComponent } from './commonComponents/competences/competences.component';
-import { TalentsComponent } from './commonComponents/talents/talents.component';
-import { LoreComponent } from './commonComponents/lore/lore.component';
-import { RepertoirePotionComponent } from './commonComponents/repertoire-potion/repertoire-potion.component';
+import { GlobalComponent } from './pages/personnages/pj/global/global.component';
+import { BiographieComponent } from './pages/personnages/pj/biographie/biographie.component';
+import { TitresComponent } from './pages/personnages/pj/titres/titres.component';
+import { ModificateursComponent } from './pages/personnages/pj/modificateurs/modificateurs.component';
+import { CompetencesComponent } from './pages/personnages/pj/competences/competences.component';
+import { TalentsComponent } from './pages/personnages/pj/talents/talents.component';
+import { LoreComponent } from './pages/personnages/pj/lore/lore.component';
+import { RepertoirePotionComponent } from './pages/personnages/pj/repertoire-potion/repertoire-potion.component';
 import { PnjComponent } from './pages/personnages/pnj/pnj.component';
-import { RencontreComponent } from './commonComponents/rencontre/rencontre.component';
+import { RencontreComponent } from './pages/personnages/pj/rencontre/rencontre.component';
 import { PersonnagesComponent } from './pages/personnages/personnages.component';
-import { SurnomComponent } from './commonComponents/global/surnom/surnom.component';
-import { CaracteristiquesComponent } from './commonComponents/global/caracteristiques/caracteristiques.component';
+import { SurnomComponent } from './pages/personnages/pj/global/surnom/surnom.component';
+import { CaracteristiquesComponent } from './pages/personnages/pj/global/caracteristiques/caracteristiques.component';
 
 @NgModule({
   declarations: [
