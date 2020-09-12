78ef45e84636b7c64fbd2bca32bcbc85df2b56a7
lePitch <pierre.henri.jautard@gmail.com>
2020-09-12 10:52:55 +0200

    feat: contes

--- src/app/pages/maps/map/contes/contes.component.ts
+++ src/app/pages/maps/map/contes/contes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
+import { ContesEtLgendes } from 'src/app/models/Maps';
 
 @Component({
   selector: 'app-contes',
   templateUrl: './contes.component.html',
   styleUrls: ['./contes.component.scss']
 })
-export class ContesComponent implements OnInit {
+export class ContesComponent implements OnChanges {
+
+  @Input() contes: ContesEtLgendes;
+  c: string[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    for (const key in this.contes) {
+      if (Object.prototype.hasOwnProperty.call(this.contes, key)) {
+        const element = this.contes[key];
+        this.c.push(key);
+        this.c.push(element);
+      }
+    }
   }
 
 }
