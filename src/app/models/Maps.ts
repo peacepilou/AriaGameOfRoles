ab3e58b5ffb30d91fa57924a3b96a7fdc56bf091
lePitch <pierre.henri.jautard@gmail.com>
2020-09-14 14:31:29 +0200

    fix description object

--- src/app/models/Maps.ts
+++ src/app/models/Maps.ts
@@ -103,13 +103,14 @@ export interface Gographies {
 
 
 export interface Description {
-  description1: string;
-  description2: string;
-  description3: string;
-  description4: string;
-  description5: string;
-  description6: string;
-  description7: string;
+  description?: string;
+  description1?: string;
+  description2?: string;
+  description3?: string;
+  description4?: string;
+  description5?: string;
+  description6?: string;
+  description7?: string;
 }
 
 export interface CitDeVarna {
