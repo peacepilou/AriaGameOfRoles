b13cf2aba1ad2f46a344d6aa3d8f1455ae71fd71
lePitch <pierre.henri.jautard@gmail.com>
2020-09-16 11:17:50 +0200

    rebase finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,8 @@ import { MapsComponent } from './pages/maps/maps.component';
 
 
 const routes: Routes = [
-<<<<<<< HEAD
   {path: '', component: PersonnagesComponent}
   // {path: '', component: MapsComponent}
-=======
-  // {path: '', component: PersonnagesComponent}
-  {path: '', component: MapsComponent}
->>>>>>> ab3e58b5ffb30d91fa57924a3b96a7fdc56bf091
 ];
 
 @NgModule({
